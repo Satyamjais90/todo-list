6fa7f3e3ef2d45add80a1971a37ba4e1eeae0c81
Satyamjais90 <93418053+Satyamjais90@users.noreply.github.com>
2022-06-26 23:00:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import "./styles.css";
+
+import "./App.css";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>Todo List</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
